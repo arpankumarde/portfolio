40244a5c5af990c01c65c71903978cdce3e8628f
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-08-20 22:24:19 +0530

    feat: SEO optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,11 @@ import "./globals.css";
 import SocialWidget from "@/components/blocks/social_widget";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import seo, { jsonLd } from "@/data/seo";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Arpan's Portfolio",
-  description: "Web, Cloud and AI Builder",
-};
+export const metadata: Metadata = seo;
 
 export default function RootLayout({
   children,
@@ -19,6 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          id="ldjson"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
+          }}
+        />
+      </head>
+
       <body
         className={`${inter.className} text-white dark bg-gradient-to-tr from-60% to-red-500/10`}
       >
