69e47d3300796992bfb93d2fa15716074251bbdc
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-08-30 22:16:06 +0530

    feat: bind microsoft clarity analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,17 @@ export default function RootLayout({
             __html: JSON.stringify(jsonLd).replace(/</g, "\\u003c"),
           }}
         />
+        <script
+          id="ms-analytics"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+                          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                      })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");`,
+          }}
+        />
       </head>
 
       <body
